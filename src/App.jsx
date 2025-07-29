171006366a791cbc7b935a179dcfe8cce02a0af5
Munch <Berg1213@users.noreply.github.com>
2025-07-29 16:12:07 -0700

    Adds PopularContent to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
-import AlbumViewer from "./components/AlbumViewer";
+import PopularContent from "./components/PopularContent";
 import Playlist from "./components/Playlist";
 import SearchBar from "./components/SearchBar";
 import SearchResults from "./components/SearchResults";
 import Track from "./components/Track";
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState(null);
+  const [popularData, setPopularData] = useState(null);
 
   return (
-    <>
-    </>
-  )
+    <div>
+      <SearchBar onSearch={handleSearch} />
+      {searchResults ? (
+        <SearchResults data={searchResults} />
+      ) : (
+        <PopularContent data={popularData} />
+      )}
+    </div>
+  );
 }
-
 export default App
