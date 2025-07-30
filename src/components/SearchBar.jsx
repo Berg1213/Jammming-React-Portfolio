364c9924f1866b1096bd9b5d199196402c2968df
Munch <Berg1213@users.noreply.github.com>
2025-07-30 16:21:06 -0700

    WIP: Implements search bar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,19 @@
+function SearchBar({ searchTerm, setSearchTerm, handleSearch }) {
 
-function SearchBar() {
-
-  return (<></>);
+  return (
+    <>
+        <div className="search">
+          <form onSubmit={handleSearch} className="search-form">
+            <input 
+                type="text" 
+                placeholder="search" 
+                className="search-input" 
+                value={searchTerm} 
+                onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            <button type="submit" className="search-button">Search</button>
+          </form>
+        </div>
+    </>
+  );
 }
-
-
-export default SearchBar;
