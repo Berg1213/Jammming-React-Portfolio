35cf802de9cea6d0077c8847553360d6f7af2d2e
Munch <Berg1213@users.noreply.github.com>
2025-07-30 11:26:45 -0700

    Adds popular content state management and API integration

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,28 @@ import SearchBar from "./components/SearchBar";
 import SearchResults from "./components/SearchResults";
 import Track from "./components/Track";
 
+import { useState,useEffect } from "react";
+
+import { getTopArtists,getTopTracks } from "./utils/LastFMAPI";
+
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState(null);
   const [popularData, setPopularData] = useState(null);
+
+  const [popularLoading, setPopularLoading] = useState(true);
+  //const [searchLoading, setSearchLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchPopularData = async () => {
+      const [artists, tracks] = await Promise.all([
+        getTopArtists(), getTopTracks()
+      ]);
+      setPopularData({ artists, tracks});
+    };
+
+    fetchPopularData();
+  }, []);
 
   return (
     <div>
@@ -16,7 +34,11 @@ function App() {
       {searchResults ? (
         <SearchResults data={searchResults} />
       ) : (
-        <PopularContent data={popularData} />
+        <PopularContent 
+          artists={popularData?.artists} 
+          tracks={popularData?.tracks} 
+          loading={popularLoading}
+        />
       )}
     </div>
   );
