fa8a99e4fbc69a73d7475c73aed2d65dce3b04b6
Munch <Berg1213@users.noreply.github.com>
2025-07-31 17:58:09 -0700

    Fixes bugs in PopularContent

--- src/components/PopularContent.jsx
+++ src/components/PopularContent.jsx
@@ -1,15 +1,41 @@
 function PopularContent({ tracks, artists, loading }) {
+
+  console.log('Artists received:', artists);
+  console.log('Tracks received:', tracks);
+  
+  if (artists && artists.length > 0) {
+    console.log('First artist:', artists[0]);
+    console.log('First artist image array:', artists[0]?.image);
+    console.log('First artist image object:', artists[0].image[2]);
+    console.log('Image array length:', artists[0]?.image?.length);
+    console.log('All artist images:', artists[0].image);
+
+  }
+  
+  if (tracks && tracks.length > 0) {
+    console.log('First track:', tracks[0]);
+    console.log('First track image array:', tracks[0]?.image);
+    console.log('First track image object:', tracks[0].image[2]);
+    console.log('All track images:', tracks[0].image);
+
+  }
   if (loading) {
     return <div>Loading popular content data...</div>;
+  };
+
+  const isPlaceholder = (imageUrl) => {
+    return imageUrl?.includes('2a96cbd8b46e442fc41c2b86b821562f.png');
   };
 
   return (
     <div className="popular-content">
       <div className="popular-tracks">{tracks?.map(track => (
         <div key={track.url}>
-          <img src={track.image[2]['#text']} alt={track.name} />
+          {!isPlaceholder(track.image[2]?.['#text']) && (
+            <img src={track.image[2]?.['#text']} alt={track.name} />
+          )}
           <h3>{track.name}</h3>
-          <p>by {track.artist}</p>
+          <p>by {track.artist?.name || track.artist}</p>
           <p>{track.playcount} plays</p>
           <p>{track.listeners} listeners</p>
           <a href={track.url}>View on Last.fm</a>
@@ -18,7 +44,9 @@ function PopularContent({ tracks, artists, loading }) {
       </div>
       <div className="popular-artists">{artists?.map(artist => (
         <div key={artist.url}>
-          <img src={artist.image[2]['#text']} alt={artist.name} />
+          {!isPlaceholder(artist.image[2]?.['#text']) && (
+            <img src={artist.image[2]?.['#text']} alt={artist.name} />
+          )}
           <h3>{artist.name}</h3>
           <p>{artist.playcount} plays</p>
           <p>{artist.listeners} listeners</p>
